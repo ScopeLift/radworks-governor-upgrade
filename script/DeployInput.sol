a5b2b159d721ae0c8c10de6ca4070c3c9175266b
John Feras <jferas@ferasinfotech.com>
2023-11-09 10:01:58 -0500

    Fixed config parameters and compiler version in DeployInput.sol

--- script/DeployInput.sol
+++ script/DeployInput.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: AGPL-3.0-only
-pragma solidity ^0.8.18;
+pragma solidity ^0.8.20;
 
 contract DeployInput {
   uint256 constant INITIAL_VOTING_DELAY = 7200; // 24 hours
-  uint256 constant INITIAL_VOTING_PERIOD = 28_800; // matches existing config
-  uint256 constant INITIAL_PROPOSAL_THRESHOLD = 10_000e18; // matches existing config
+  uint256 constant INITIAL_VOTING_PERIOD = 17_280; // matches existing config
+  uint256 constant INITIAL_PROPOSAL_THRESHOLD = 1_000_000e18; // matches existing config
 }
