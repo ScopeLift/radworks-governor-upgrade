6ddeca09ea0d4d4179a2bd9926e8a32aab71b3d2
John Feras <jferas@ferasinfotech.com>
2024-01-08 16:37:05 -0500

    Cleanup test names / scopelint fix (#22)
    Cleaned up test contracts and how they are organized and named for better compatibility with "scopelint spec" command.

--- test/Constants.sol
+++ test/Constants.sol
@@ -7,7 +7,6 @@ contract Constants {
   address constant TIMELOCK = 0x8dA8f82d2BbDd896822de723F55D6EdF416130ba;
   address constant DRIPS = 0xd0Dd053392db676D57317CD4fe96Fc2cCf42D0b4;
 
-  // TODO: resolve the list of large delegates with tallyaddress
   address constant PROPOSER = 0x464D78a5C97A2E2E9839C353ee9B6d4204c90B0b; // cloudhead.eth
 
   address constant USDC_ADDRESS = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
@@ -16,8 +15,8 @@ contract Constants {
   address constant MTV_ADDRESS = 0x6226e00bCAc68b0Fe55583B90A1d727C14fAB77f;
   uint256 constant MAX_REASONABLE_TIME_PERIOD = 302_400; // 6 weeks assume a 12 sec block time
 
-  // we have not yet deployed the Radworks Bravo Governor
-  address constant DEPLOYED_BRAVO_GOVERNOR = 0x1111111111111111111111111111111111111111;
+  // we have not yet deployed the Radworks Bravo Governor (will be replaced with deployed address)
+  address constant DEPLOYED_BRAVO_GOVERNOR = address(0x0);
 
   uint256 constant QUORUM = 4_000_000e18;
 }
